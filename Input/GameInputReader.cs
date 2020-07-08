e58a30dfce416cc00711e672c52f608ae31fa8b7
JohnPetersons <john.w.petersons@gmail.com>
2020-07-08 13:43:49 -0400

    Slightly less repeated stuff

--- Input/GameInputReader.cs
+++ Input/GameInputReader.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace GenericUnityGame {
+    /*
+    button inputs for the first player/controller would be a1, b1, x1, y1, etc.
+    */
     public class GameInputReader : GameEventListener {
 
         private static readonly string[] buttonInputs = {"a", "b", "x", "y", "leftTrigger", "leftBumper", "rightTrigger", "rightBumper", "start", "select"};
         private static readonly string[] axisInputs = {"leftStickUpDown", "rightStickUpDown", "leftStickLeftRight", "rightStickLeftRight", "dPadUpDown", "dPadLeftRight"};
         private Dictionary<string, string> inputMapping = new Dictionary<string, string>();
         private string playerNumber;
+        private int playerInput;
         public new void Begin() {
             base.Begin();
         }
@@ -17,19 +21,19 @@ namespace GenericUnityGame {
             base.Tick();
             foreach(string str in GameInputReader.buttonInputs) {
                 string temp = str + playerNumber;
-                string tempInput = "input" + playerNumber;
                 if (Input.GetButtonDown(temp) && inputMapping.ContainsKey(temp)) {
-                    new TypedGameEvent<string>(tempInput, inputMapping[temp], "start");
+                    new TypedGameEvent<string>(playerInput, inputMapping[temp], "start");
                 } else if (Input.GetButton(temp) && inputMapping.ContainsKey(temp)) {
-                    new TypedGameEvent<string>(tempInput, inputMapping[temp], "held");
+                    new TypedGameEvent<string>(playerInput, inputMapping[temp], "held");
                 } else if (Input.GetButtonUp(temp) && inputMapping.ContainsKey(temp)) {
-                    new TypedGameEvent<string>(tempInput, inputMapping[temp], "end");
+                    new TypedGameEvent<string>(playerInput, inputMapping[temp], "end");
                 }
             }
         }
 
         public void SetPlayerNumber(int i) {
-            playerNumber = "" + i;
+            playerNumber = i;
+            playerInput = "input" + i;
         }
 
         public void SetInputMapping(string input, string mapping) {
