a29e5ec6eab5e96937d396876f4846c4ca73cf3d
JohnPetersons <john.w.petersons@gmail.com>
2020-07-10 13:23:53 -0400

    Change some literals to constants

--- StateMachines/Input/GameInputState.cs
+++ StateMachines/Input/GameInputState.cs
@@ -11,6 +11,9 @@ namespace GenericUnityGame {
 
         private static readonly string[] BUTTON_INPUTS = {"a", "b", "x", "y", "leftTrigger", "leftBumper", "rightTrigger", "rightBumper", "start", "select"};
         private static readonly string[] AXIS_INPUTS = {"leftStickUpDown", "rightStickUpDown", "leftStickLeftRight", "rightStickLeftRight", "dPadUpDown", "dPadLeftRight"};
+        public const string START = "start";
+        public const string HELD = "held";
+        public const string END = "end";
         private Dictionary<string, string> inputMapping = new Dictionary<string, string>();
         private int playerNumber;
 
@@ -27,11 +30,11 @@ namespace GenericUnityGame {
             foreach(string str in GameInputState.BUTTON_INPUTS) {
                 string temp = str + playerNumber;
                 if (Input.GetButtonDown(temp) && inputMapping.ContainsKey(temp)) {
-                    new TypedGameEvent<string>(this.GetEventListenerId(), inputMapping[temp], "start");
+                    new TypedGameEvent<string>(this.GetEventListenerId(), inputMapping[temp], GameInputState.START);
                 } else if (Input.GetButton(temp) && inputMapping.ContainsKey(temp)) {
-                    new TypedGameEvent<string>(this.GetEventListenerId(), inputMapping[temp], "held");
+                    new TypedGameEvent<string>(this.GetEventListenerId(), inputMapping[temp], GameInputState.HELD);
                 } else if (Input.GetButtonUp(temp) && inputMapping.ContainsKey(temp)) {
-                    new TypedGameEvent<string>(this.GetEventListenerId(), inputMapping[temp], "end");
+                    new TypedGameEvent<string>(this.GetEventListenerId(), inputMapping[temp], GameInputState.END);
                 }
             } 
             foreach(string str in GameInputState.AXIS_INPUTS) {
@@ -45,7 +48,11 @@ namespace GenericUnityGame {
 
         public void SetInputMapping(string input, string mapping) {
             if (inputMapping.ContainsKey(input)) {
-                inputMapping[input] = mapping;
+                if (mapping == null || mapping.Length <= 0) {
+                    inputMapping.Remove(input);
+                } else {
+                    inputMapping[input] = mapping;
+                }
             } else {
                 inputMapping.Add(input, mapping);
             }
