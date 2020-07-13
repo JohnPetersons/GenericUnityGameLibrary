62fcada5abc4a5769bdd9967c3e1c995df0e7622
JohnPetersons <john.w.petersons@gmail.com>
2020-07-13 13:34:50 -0400

    Handling OnTrigger events

---
+++ Events/Collision/GameOnTriggerHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GenericUnityGame {
+    
+    public class GameOnTriggerHandler : GameEventListener {
+
+        private List<GameObject> currentCollisions;
+
+        public new void Begin() {
+            base.Begin();
+            this.currentCollisions = new List<Collider>();
+        }
+
+        public new void Tick() {
+            base.Tick();
+            if (this.currentCollisions.Count > 0) {
+                foreach(GameObject go in this.currentCollisions) {
+                    new TypedGameEvent<GameObject>(this.GetListenerId(), "triggerStay", go);
+                }
+            }
+        }
+
+        private void OnTriggerEnter(Collider other) {
+            new TypedGameEvent<GameObject>(this.GetListenerId(), "triggerEnter", other.gameObject);
+            this.currentCollisions.Add(other.gameObject);
+        } 
+
+        private void OnTriggerExit(Collider other) {
+            new TypedGameEvent<GameObject>(this.GetListenerId(), "triggerExit", other.gameObject);
+            this.currentCollisions.Remove(other.gameObject);
+        }
+    }
+}
