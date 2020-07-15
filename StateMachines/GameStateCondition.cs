93a8447f3ef1fe0837fe8d354af00980a403f90a
JohnPetersons <john.w.petersons@gmail.com>
2020-07-14 20:55:04 -0400

    adding game object reference to state conditions

--- StateMachines/GameStateCondition.cs
+++ StateMachines/GameStateCondition.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace GenericUnityGame {
     public abstract class GameStateCondition {
 
         private GameEventListenerId sendTo;
         private string eventOnSuccess;
+        protected GameObject gameObject;
 
         private List<string> listensFor;
         public GameStateCondition(GameEventListenerId sendTo, string eventOnSuccess) {
+            this.gameObject = GameSystem.GetGameData<GameObject>(sendTo.GetListenerId());
             this.sendTo = sendTo;
             this.eventOnSuccess = eventOnSuccess;
             this.listensFor = new List<string>();
