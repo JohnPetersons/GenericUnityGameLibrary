9e9b751a5d9e4857f748745729bb2f2c40dfd68c
JohnPetersons <john.w.petersons@gmail.com>
2020-07-14 20:28:36 -0400

    minor update to event listener states

--- StateMachines/GameEventListenerState.cs
+++ StateMachines/GameEventListenerState.cs
@@ -6,8 +6,10 @@ namespace GenericUnityGame {
     public class GameEventListenerState: GameState
     { 
         private GameEventListenerId listenerId;
+        protected GameObject gameObject;
         public GameEventListenerState(GameEventListenerId listenerId): base() {
             this.listenerId = listenerId;
+            this.gameObject = GameSystem.GetGameData<GameObject>(listenerId.GetListenerId());
         }
 
         public override void Begin() {
