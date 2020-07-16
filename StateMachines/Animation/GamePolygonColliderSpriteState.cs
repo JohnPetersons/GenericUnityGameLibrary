1288e436933f3007d4a1f442265554d75edc4a56
JohnPetersons <john.w.petersons@gmail.com>
2020-07-16 16:47:53 -0400

    GamePolygonColliderSpriteState

---
+++ StateMachines/Animation/GamePolygonColliderSpriteState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GenericUnityGame {
+    public class GamePolygonColliderSpriteState : GameSpriteState {
+        
+        private Vector2[] colliderPoints;
+        private PolygonCollider2D collider;
+
+        public GamePolygonColliderSpriteState(GameEventListenerId listenerId, string spriteName): base(listenerId, spriteName) {
+            this.colliderPoints = new Vector2[] {new Vector2(0.5f, 0.5f),
+                new Vector2(0.5f, -0.5f),
+                new Vector2(-0.5f, -0.5f),
+                new Vector2(-0.5f, 0.5f)}; 
+            this.collider = this.gameObject.GetComponent<PolygonCollider2D>();
+        }
+
+        public GamePolygonColliderSpriteState(GameEventListenerId listenerId, string spriteName, Vector2[] colliderPoints): base(listenerId, spriteName) {
+            this.colliderPoints = colliderPoints; 
+            this.collider = this.gameObject.GetComponent<PolygonCollider2D>();
+        }
+
+        public override void Begin() {
+            base.Begin();
+            this.collider.SetPath(0, this.colliderPoints);
+        }
+    }
+}
