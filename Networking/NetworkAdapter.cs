4c32fc87e4257d9d91e5f5856e08f88bb5dc1f8b
JohnPetersons <john.w.petersons@gmail.com>
2020-07-18 13:52:34 -0400

    Online Multiplayer WIP

---
+++ Networking/NetworkAdapter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkAdapter : NetworkBehaviour
+{
+
+    private bool networking;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Begin();
+    }
+
+    public virtual void Begin() {
+        this.networking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Tick();
+    }
+
+    public virtual void Tick() {
+
+    }
+
+    public bool IsLocal() {
+        return this.isLocalPlayer || !this.networking;
+    }
+}
