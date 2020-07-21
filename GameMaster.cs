93dcbfd4ed2586aae7e4c306dad8a815804707f4
JohnPetersons <john.w.petersons@gmail.com>
2020-07-21 14:23:06 -0400

    loading settings

--- GameMaster.cs
+++ GameMaster.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 namespace GenericUnityGame {
     /*
@@ -19,8 +20,32 @@ namespace GenericUnityGame {
             this.SetListenerId(GameMaster.TAG);
             GameSystem.SetTimeMultiplier("default", 1.0);
             this.loader = new GameLoader();
+            GameSystem.SetGameData<GameLoader>("GameLoader", this.loader);
+            GameSystem.SetGameData<GameLoader>("MenuLoader", new GameLoader());
             this.loader.LoadFile("MainMenu");
             this.loaded = false;
+            Dictionary<string, float> settingsList = new Dictionary<string, float>();
+            if (File.Exists(@"settings.txt")) {
+                foreach (string line in File.ReadLines(@"settings.txt")) {
+                    settingsList.Add(line.Substring(0, line.IndexOf(",")), float.Parse(line.Substring(line.IndexOf(",") + 1)));
+                }
+                Settings.charge = settingsList["charge"];
+                Settings.gameSpeed = settingsList["gameSpeed"];
+                Settings.moveForward = settingsList["moveForward"];
+                Settings.moveBackward = settingsList["moveBackward"];
+                Settings.quickStepForward = settingsList["quickStepForward"];
+                Settings.quickStepForwardTimer1 = settingsList["quickStepForwardTimer1"];
+                Settings.quickStepForwardTimer2 = settingsList["quickStepForwardTimer2"];
+                Settings.quickStepBackward = settingsList["quickStepBackward"];
+                Settings.quickStepBackwardTimer1 = settingsList["quickStepBackwardTimer1"];
+                Settings.quickStepBackwardTimer2 = settingsList["quickStepBackwardTimer2"];
+                Settings.dashChargeTimer = settingsList["dashChargeTimer"];
+                Settings.dash = settingsList["dash"];
+                Settings.dashTimer = settingsList["dashTimer"];
+                Settings.chargeRecoveryTimer = settingsList["chargeRecoveryTimer"];
+                Settings.collisionLoss = settingsList["collisionLoss"];
+                Settings.collisionWin = settingsList["collisionWin"];
+            }
         }
 
         // Update is called once per frame
